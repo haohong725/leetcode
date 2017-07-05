100fa412c55dfff83e5fc54ed7c2c49424b5ca33
haohong725 <haohong725@qq.com>
2017-07-05 20:32:24 +0800

    x

---
+++ 013.roman-to-integer-todo/Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public int romanToInt(String s) {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(444);
+    }
+}
---
+++ 014.longest-common-prefix/Solution.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length < 1) {
+            return "";
+        }
+
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+            prefix = this.commonPrefix(prefix, strs[i]);
+        }
+
+        return prefix;
+    }
+
+    private String commonPrefix(String a, String b) {
+        if (a == null || b == null) {
+            return "";
+        }
+
+        int minLength = Math.min(a.length(), b.length());
+
+        String prefix = "";
+        for (int i = 0; i <= minLength; i++) {
+            if (! b.startsWith(a.substring(0, i))) {
+                break;
+            }
+
+            prefix = a.substring(0, i);
+        }
+
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        String[] sArr = new String[0];
+//        sArr[0] = "Cheese";
+//        sArr[1] = "Cheese2";
+//        sArr[2] = "Chees2";
+//        sArr[3] = "Chees3";
+//        sArr[4] = "Cheex";
+
+        String result = s.longestCommonPrefix(sArr);
+
+        System.out.println(result);
+
+//        System.out.println(s.commonPrefix("abcdefg", "abcde"));
+
+//        System.out.println("abcdefg".substring(0, 2));
+    }
+}
