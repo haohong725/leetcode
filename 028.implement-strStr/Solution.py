69e83b345e09046aa155b816396bab385d52052a
haohong725 <haohong725@qq.com>
2017-07-10 21:16:16 +0800

    amend

---
+++ 028.implement-strStr/Solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        print "strStr method"
+
+if __name__ == '__main__':
+    s = Solution();
+    s.strStr("hello world", "world")
+
+
+
---
+++ 035.search-insert-position/Solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int i;
+
+        for (i = 0; i < nums.length; i++) {
+            if (nums[i] >= target) {
+                break;
+            }
+        }
+
+        return i;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] nums = new int[]{1, 3, 5, 6};
+        int target = 0;
+
+        System.out.println(s.searchInsert(nums, target));
+    }
+}
