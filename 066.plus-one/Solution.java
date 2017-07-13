be3ae892b03fd5ec3bf5cadbf41291b56752ad76
haohong725 <haohong725@qq.com>
2017-07-13 08:36:01 +0800

    x

---
+++ 066.plus-one/Solution.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        int length = digits.length;
+        for (int i = length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i] += 1;
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        int[] newDigit = new int[length + 1];
+        newDigit[0] = 1;
+
+        return newDigit;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+
+        System.out.println(Arrays.toString(s.plusOne(new int[]{1,2, 3, 4})));
+        System.out.println(Arrays.toString(s.plusOne(new int[]{4, 9, 9, 9})));
+        System.out.println(Arrays.toString(s.plusOne(new int[]{9, 9, 9, 9})));
+    }
+}
