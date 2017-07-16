6d961f9d80503d771a38d795218d18317641350d
haohong725 <haohong725@qq.com>
2017-07-16 21:49:27 +0800

    add cache

--- 070.climbing-stairs/Solution.java
+++ 070.climbing-stairs/Solution.java
@@ -1,6 +1,7 @@
 public class Solution {
     public int climbStairs(int n) {
-        return this._climbStairs(0, n);
+        int[] cache = new int[n+1];
+        return this._climbStairs(0, n, cache);
     }
 
     /**
@@ -8,19 +9,22 @@ public class Solution {
      * @param n 剩余的步数
      * @return
      */
-    private int _climbStairs(int s, int n) {
+    private int _climbStairs(int s, int n, int[] cache) {
         if (s > n) {
             return 0;
         } else if (s == n) {
             return 1;
+        } else if (cache[s] > 0) {
+            return cache[s];
         } else {
-            return this._climbStairs(s+1, n) + this._climbStairs(s+2, n);
+            cache[s] = this._climbStairs(s+1, n, cache) + this._climbStairs(s+2, n, cache);
+            return cache[s];
         }
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
 
-        System.out.println(s.climbStairs(50));
+        System.out.println(s.climbStairs(30));
     }
 }
