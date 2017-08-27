d3595ac2baecf4bb2aa35132e38bcd5aaf9aedb2
haohong725 <haohong725@qq.com>
2017-08-27 10:33:51 +0800

    x

---
+++ 104.maximum-depth-of-binary-tree/Solution.java
@@ -0,0 +1,63 @@
+import java.lang.Math;
+
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left == null && root.right == null) {
+            return 1;
+        } else if (root.left == null && root.right != null) {
+            return 1 + this.maxDepth(root.right);
+        } else if (root.left != null && root.right == null) {
+            return 1 + this.maxDepth(root.left);
+        } else {
+            return 1 + Math.max(this.maxDepth(root.left), this.maxDepth(root.right));
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        TreeNode root = s.mock1();
+        System.out.println(s.maxDepth(root));
+
+    }
+
+    private TreeNode mock1() {
+        // 第一层
+        TreeNode treeRoot = new TreeNode(1);
+
+        // 第二层
+//        treeRoot.left = new TreeNode(2);
+//        treeRoot.right = new TreeNode(3);
+
+        // 第三层
+//        treeRoot.left.left = new TreeNode(4);
+//        treeRoot.left.right = new TreeNode(5);
+//        treeRoot.right.left = new TreeNode(6);
+//        treeRoot.right.right = new TreeNode(7);
+
+        return treeRoot;
+    }
+
+    public void printTree(TreeNode treeNode) {
+        System.out.print(treeNode.val + " ");
+
+        if (treeNode.left != null) {
+            printTree(treeNode.left);
+        }
+
+        if (treeNode.right != null) {
+            printTree(treeNode.right);
+        }
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
