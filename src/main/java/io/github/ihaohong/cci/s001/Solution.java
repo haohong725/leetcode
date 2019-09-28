d9dfaa4f5e9e52e5ffbfefa6a48ba462e08f2690
haohong725 <haohong725@qq.com>
2019-09-28 23:41:24 +0800

    x

--- src/main/java/io/github/ihaohong/cci/s001/Solution.java
+++ src/main/java/io/github/ihaohong/cci/s001/Solution.java
@@ -1,5 +1,11 @@
 package io.github.ihaohong.cci.s001;
 
+/**
+ * 实验一个算法，确定一个字符串的所有字符是否全都不同。不允许使用新的数据结构。
+ *
+ * 注意：1. 需要确定字符范围，ASCII，还是 Unicode
+ * 时间复杂度O(n),空间复杂度O(1)
+ */
 public class Solution {
     public static void main(String[] args) {
         System.out.println("hello");
